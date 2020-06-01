2515bc46ee0b931a87298cb08690e98ae6d49847
ltorbay <louistorbay@gmail.com>
2020-06-01 12:25:26 +0200

    feature: test geo encoding using postgis

--- src/main/java/io/ambulante/backend/model/dto/Producer.java
+++ src/main/java/io/ambulante/backend/model/dto/Producer.java
@@ -1,6 +1,7 @@
 package io.ambulante.backend.model.dto;
 
 import lombok.Data;
+import org.springframework.data.geo.Point;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public class Producer {
     private String name;
     private List<Article> articles;
     private List<PresentationItem> presentationList;
+
+    private Point coordinates;
 }
--- src/main/java/io/ambulante/backend/model/entity/ProducerEntity.java
+++ src/main/java/io/ambulante/backend/model/entity/ProducerEntity.java
@@ -2,8 +2,11 @@ package io.ambulante.backend.model.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Type;
+import org.springframework.data.geo.Point;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -18,7 +21,10 @@ import java.util.UUID;
 public class ProducerEntity extends AbstractEntity {
     private UUID userId;
     private String name;
-    private String geolocation;
+
+    @Column(columnDefinition = "Geometry")
+    @Type(type = "org.hibernate.spatial.GeometryType")
+    private Point coordinates;
 
     @JoinColumn(name = "producer", referencedColumnName = "id", nullable = false)
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
