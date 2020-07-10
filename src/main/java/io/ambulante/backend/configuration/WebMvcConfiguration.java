1b4d893b524217c702268edf7e175015eec78717
ltorbay <louistorbay@gmail.com>
2020-07-10 11:28:30 +0200

    task: added cors configuration again

--- src/main/java/io/ambulante/backend/configuration/WebMvcConfiguration.java
+++ src/main/java/io/ambulante/backend/configuration/WebMvcConfiguration.java
@@ -1,14 +1,25 @@
 package io.ambulante.backend.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
+    private final LogInterceptor logInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Explicitly allow all origin requests
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
     }
 }
