2603c9cc99c477f9f01c89174574ee2019ecf9f7
ltorbay <louistorbay@gmail.com>
2020-09-17 15:40:22 +0200

    Add audit to entities

--- src/main/java/io/ambulante/backend/model/entity/AbstractEntity.java
+++ src/main/java/io/ambulante/backend/model/entity/AbstractEntity.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Data
 @MappedSuperclass
@@ -12,4 +16,17 @@ public abstract class AbstractEntity {
     @Id
     @GeneratedValue
     private Integer id;
+
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+    @PrePersist
+    protected void onCreate() {
+        created = LocalDateTime.now(ZoneOffset.UTC);
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = LocalDateTime.now(ZoneOffset.UTC);
+    }
 }
